b772a93f930a8820229f41190959cf8211f8e985
Hoyong Jung <jhoyong57@gmail.com>
2018-10-14 19:57:09 -0600

    Hoyong made Home Index page and little bit of Stack page

--- 403_HWTwo/Controllers/StackController.cs
+++ 403_HWTwo/Controllers/StackController.cs
@@ -8,10 +8,74 @@ namespace _403_HWTwo.Controllers
 {
     public class StackController : Controller
     {
+        static Stack<string> MyStack = new Stack<string>();
+
         // GET: Stack
         public ActionResult Index()
         {
+            ViewBag.Mystack = MyStack;
+
             return View();
+        }
+
+        public ActionResult AddOne()
+        {
+            MyStack.Push("New Entry " + (MyStack.Count + 1));
+
+            ViewBag.MyStack = MyStack;
+
+            return View("Index");
+        }
+
+        public ActionResult AddHuge()
+        {
+            MyStack.Clear();
+
+            int i = 0;
+
+            while (i < 2000) 
+            {
+                MyStack.Push("New Entry " + (MyStack.Count + 1));
+                i++;
+            }
+
+            ViewBag.MyStack = MyStack;
+
+            return View("Index");
+        }
+
+        public ActionResult Display()
+        {
+            ViewBag.MyStack += "<table>";
+
+            foreach (var AddString in MyStack)
+            {
+                ViewBag.Mystack += "<tr>";
+                ViewBag.MyStack += "<td>";
+            }
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        public ActionResult Clear()
+        {
+            MyStack.Clear();
+            ViewBag.MyStack = MyStack;
+            return View("Index");
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            return RedirectToAction("Index", "Home");
         }
     }
 }
